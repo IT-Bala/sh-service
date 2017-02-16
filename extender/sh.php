36dd6826631ca4a1dc7691534567556a48c84a0c
kutung design pvt ltd <balasundaram@kutung.in>
2017-02-16 16:19:04 +0530

    Changes

--- extender/sh.php
+++ extender/sh.php
@@ -8,3 +8,29 @@ Http::get('/sha',function($app){
 
 Http::routes('GET /sh',['a'=>'User::insert']);
 
+Http::page('/admin',function($app){
+    if(isset($_POST['save'])){
+        
+        if(!$app->db->has("tbl_routes",["route"=>$app->clean_url($_POST['title'])])){       
+            $id = $app->db->insert("tbl_routes", [
+                    "type" => "page",
+                    "route" => $app->clean_url($_POST['title']),
+                    "when_created" => date('Y-m-d H:i:s')
+                ]);
+            #$id = $app->db->id();
+              
+            $app->db->insert("tbl_pages", [
+                            "route_id" => $id,
+                            "title" => $_POST['title'],
+                            "content" => $_POST['content'],
+                            "when_created" => date('Y-m-d H:i:s')
+                        ]);
+            echo "Submitted successfuly!";
+      }else{
+            echo "Already exist route!";
+      }
+                
+    }
+    $app->html('admin');    
+});
+

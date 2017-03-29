49a8a9f08c54151b1849023f7f4fd76f884a79a1
phpbala <32deva@gmail.com>
2017-03-29 14:27:11 +0530

    Update commands.php

--- cli/commands.php
+++ cli/commands.php
@@ -2,11 +2,18 @@
 require_once 'cli-terminal.php';
 $colors = new Colors();
 // Test some basic printing with Colors class
-echo $colors->getColoredString("[ Command Options ]", "yellow", "") . "\n\n";
+echo clean_color($colors->getColoredString("[ Command Options ]", "yellow", "") . "\n\n");
 
-echo $colors->getColoredString("controller:controllername", "green", "") . "	Create a controller\n";
-echo $colors->getColoredString("model:modelname		", "green", "") . "	Create a model\n";
-echo $colors->getColoredString("library:libraryname	", "green", "") . "	Create a library\n";
-echo $colors->getColoredString("package:packagename	", "green", "") . "	Create a package\n";
-echo $colors->getColoredString("extender:extendername	", "green", "") . "	Create a extender\n";
+echo clean_color($colors->getColoredString("controller:controllername", "green", "") . "	Create a controller\n");
+echo clean_color($colors->getColoredString("model:modelname		", "green", "") . "	Create a model\n");
+echo clean_color($colors->getColoredString("library:libraryname	", "green", "") . "	Create a library\n");
+echo clean_color($colors->getColoredString("package:packagename	", "green", "") . "	Create a package\n");
+echo clean_color($colors->getColoredString("extender:extendername	", "green", "") . "	Create a extender\n");
 echo "\n";
+function clean_color($str){
+	 $codes = array("\033[0;32m", "\033[0m", "\033[0;31m","\033[0m");
+	 $rcodes = array("","","","");
+	 if(strtoupper(substr(PHP_OS, 0, 3)) != 'LIN'){
+	 	$str = str_replace($codes,$rcodes, $str);
+	 } return $str;
+}

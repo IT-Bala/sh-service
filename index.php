ea398dfa959c673cea2fcc327e28b66859265d7a
phpbala <32deva@gmail.com>
2017-03-30 18:09:15 +0530

    update index.php
    autoloader changes

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ setlocale(LC_ALL, 'en_US.UTF8');
 #==============================================#
 define("SHA",false); # Small Http::SH Authentication [Don't remove it]
 #==============================================#
+require 'autoload.php';
 require 'Http.php';
 
 $app = new Http();

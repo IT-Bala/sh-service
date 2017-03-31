18b7794b3fac88a8cc17221ddd0068491e9490f6
phpbala <32deva@gmail.com>
2017-03-31 11:39:58 +0530

    Update Curl.php
    Option for third party API in single CURL

--- Curl.php
+++ Curl.php
@@ -1,6 +1,10 @@
 <?php
 class Curl{
-	public function post($url,$post_fields=array(),$json=false){   # true means direct json format print || false means object oriented format
+	public static function post($url,$post_fields=array(),$json=false){   # true means direct json format print || false means object oriented format
+            $KEY = SH_KEY; $VALUE = SH_VALUE;
+            if(defined("API_KEY") && defined("API_TOKEN")){
+                $KEY = API_KEY; $VALUE = API_TOKEN;
+            }
 			$ch = curl_init();
             curl_setopt($ch, CURLOPT_URL,$url);
             curl_setopt($ch, CURLOPT_POST, 1);
@@ -8,7 +12,7 @@ class Curl{
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
                 'Content-Type: application/json',                                                                                
-                SH_KEY.': '.SH_VALUE)                                                                      
+                $KEY.': '.$VALUE)                                                                      
             );
             $server_output = curl_exec ($ch);
             curl_close ($ch);
@@ -18,7 +22,12 @@ class Curl{
                 return json_decode($server_output);
             }
      } 
-	 public function file_encode($name=NULL){ $base64='';
+	 public static function file_encode($name=NULL){ 
+            $KEY = SH_KEY; $VALUE = SH_VALUE;
+            if(defined("API_KEY") && defined("API_TOKEN")){
+                $KEY = API_KEY; $VALUE = API_TOKEN;
+            }
+         $base64='';
 		 if(isset($_FILES[$name]['name']) && $_FILES[$name]['name']!=''){
 			$filename = $_FILES[$name]['name'];
 			$tmp = $_FILES[$name]['tmp_name'];
@@ -27,8 +36,11 @@ class Curl{
 			$base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 		 } return $base64;
 	 }
-	 public function form_data($url,$post_fields=array(),$json=false){   # true means direct json format print || false means object oriented format
-			
+	 public static function form_data($url,$post_fields=array(),$json=false){   # true means direct json format print || false means object oriented format
+			$KEY = SH_KEY; $VALUE = SH_VALUE;
+            if(defined("API_KEY") && defined("API_TOKEN")){
+                $KEY = API_KEY; $VALUE = API_TOKEN;
+            }
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL,$url);
             curl_setopt($ch, CURLOPT_POST, 1);
@@ -36,7 +48,7 @@ class Curl{
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
                 'Content-Type: application/json',                                                                                
-                SH_KEY.': '.SH_VALUE)                                                                      
+                $KEY.': '.$VALUE)                                                                      
             );
             $server_output = curl_exec ($ch);
             curl_close ($ch);
@@ -47,13 +59,17 @@ class Curl{
             }
      }     
        
-     public function get($url,$json=false){            
+     public static function get($url,$json=false){
+            $KEY = SH_KEY; $VALUE = SH_VALUE;
+            if(defined("API_KEY") && defined("API_TOKEN")){
+                $KEY = API_KEY; $VALUE = API_TOKEN;
+            }
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL,$url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
                 'Content-Type: application/json',                                                                                
-                SH_KEY.': '.SH_VALUE)                                                                       
+                $KEY.': '.$VALUE)                                                                     
             );
             $server_output = curl_exec($ch);
             curl_close ($ch);
@@ -63,7 +79,11 @@ class Curl{
                 return json_decode($server_output);
             }
      }
-	 public function put($url,$post_fields,$json=false){            
+	 public static function put($url,$post_fields,$json=false){ 
+            $KEY = SH_KEY; $VALUE = SH_VALUE;
+            if(defined("API_KEY") && defined("API_TOKEN")){
+                $KEY = API_KEY; $VALUE = API_TOKEN;
+            }           
             $ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
@@ -71,7 +91,7 @@ class Curl{
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
                 'Content-Type: application/json',                                                                                
-                SH_KEY.': '.SH_VALUE)                                                                       
+                $KEY.': '.$VALUE)                                                                       
             );
 			$server_output  = curl_exec($ch);
 			curl_close($ch);      
@@ -81,7 +101,11 @@ class Curl{
                 return json_decode($server_output);
             }
        }
-	 public function delete($url,$post_fields,$json=false){            
+	 public static function delete($url,$post_fields,$json=false){
+            $KEY = SH_KEY; $VALUE = SH_VALUE;
+            if(defined("API_KEY") && defined("API_TOKEN")){
+                $KEY = API_KEY; $VALUE = API_TOKEN;
+            }         
             $ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
@@ -89,7 +113,7 @@ class Curl{
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
                 'Content-Type: application/json',                                                                                
-                SH_KEY.': '.SH_VALUE)                                                                       
+                $KEY.': '.$VALUE)                                                                       
             );
 			$server_output  = curl_exec($ch);
 			curl_close($ch);          

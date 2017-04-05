87b5c273f5c49e391a899b24de1477ac2e93b728
phpbala <32deva@gmail.com>
2017-04-05 17:37:26 +0530

    Update config.php
    Database Query Builder Changes

--- config.php
+++ config.php
@@ -1,6 +1,10 @@
 <?php
+#############################################
+	 # DO NOT REMOVE ANYTHONG HERE #
+#############################################
 define("SH_KEY","API");
 define("SH_VALUE","SH");
+
 
 /* CONTROLLER */
 #=================================#
@@ -10,12 +14,25 @@ define("HTML_PATH","html/");
 define("LIBRARY_PATH","library/");
 define("EXT_PATH","extender/");
 define("CMS_PATH","cms/");
+
 #=================================#
+define("APPPATH",""); 				 # __DIR__     
+define("ENVIRONMENT","");			 # DEVELOPMENT
+define("BASEPATH","");				 # APPPATH."/" 
+#=================================#
+
 /* DATABASE */
 define("HOST","localhost");
 define("USERNAME","root");
 define("PASSWORD","root");
 define("DATABASE","3p-php");
-define("DATABASE_TYPE","mysql");
-define("DB_STATUS",false);    #
+define("DATABASE_TYPE","mysqli");
+define("DB_STATUS",true); #
 
+# ADDITIONAL OPTIONS
+define("DNS","");
+define("DATABASE_PREFIX","");
+
+#############################################
+	 # DO NOT REMOVE ANYTHONG HERE #
+#############################################

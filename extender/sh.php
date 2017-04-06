47c9c418906d75a4bfdf95c855542151f17c1816
phpbala <32deva@gmail.com>
2017-04-06 14:37:32 +0530

    Update sh.php
    Query builder changes

--- extender/sh.php
+++ extender/sh.php
@@ -2,8 +2,8 @@
 if(!defined("SHA")) die("Access denied!");
 
 Http::get('/sha',function($app){
-	$q= Http::db()->query("select * from tbl_users")->fetchAll();
-	echo $app->json($q);
+	$q = Http::db()->query("select * from tbl_users");
+	echo $app->json($q->result());
 });
 
 Http::routes('GET /sh',['a'=>'User::insert']);

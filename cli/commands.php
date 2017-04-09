0b0f456efb868701137c36b71b81f303ea74e72a
phpbala <32deva@gmail.com>
2017-04-09 10:14:36 +0530

    Update commands.php

--- cli/commands.php
+++ cli/commands.php
@@ -7,7 +7,7 @@ echo clean_color($colors->getColoredString("[ Command Options ]", "yellow", "") 
 # Create Command
 echo clean_color($colors->getColoredString("[ Options ]", "brown", "") . "\n\n");
 echo clean_color($colors->getColoredString("-v , -version			", "green", "") . "	Display the version\n");
-echo clean_color($colors->getColoredString("-h , -help			", "green", "") . "	Command Help\n");
+echo clean_color($colors->getColoredString("-h , -help			", "green", "") . "	Command Help\n\n");
 
 # Create Command
 echo clean_color($colors->getColoredString("[ Create Command ]", "brown", "") . "\n");

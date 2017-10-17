ed385c5578c9523c2ff644e6e53d55d2b81803a5
phpbala <32deva@gmail.com>
2017-10-17 12:31:07 +0530

    command options updated
    command options updated

--- cli/commands.php
+++ cli/commands.php
@@ -10,7 +10,7 @@ echo clean_color($colors->getColoredString("-v , -version			", "green", "") . "	
 echo clean_color($colors->getColoredString("-h , -help			", "green", "") . "	Command Help\n\n");
 
 # Create Command
-echo clean_color($colors->getColoredString("[ Create Command ]", "brown", "") . "\n");
+echo clean_color($colors->getColoredString("[ Create Command ] create | mk", "brown", "") . "\n");
 
 echo clean_color($colors->getColoredString("create controller:controllername", "green", "") . "	Create controller\n");
 echo clean_color($colors->getColoredString("create model:modelname		", "green", "") . "	Create model\n");
@@ -19,7 +19,7 @@ echo clean_color($colors->getColoredString("create package:packagename	", "green
 echo clean_color($colors->getColoredString("create extender:extendername	", "green", "") . "	Create extender\n\n");
 
 # Remove Command
-echo clean_color($colors->getColoredString("[ Remove Command ]", "brown", "") . "\n");
+echo clean_color($colors->getColoredString("[ Remove Command ] remove | rm", "brown", "") . "\n");
 
 echo clean_color($colors->getColoredString("remove controller:controllername", "green", "") . "	Remove controller\n");
 echo clean_color($colors->getColoredString("remove model:modelname		", "green", "") . "	Remove model\n");
@@ -28,9 +28,35 @@ echo clean_color($colors->getColoredString("remove package:packagename	", "green
 echo clean_color($colors->getColoredString("remove extender:extendername	", "green", "") . "	Remove extender\n");
 echo clean_color($colors->getColoredString("remove module:modulename	", "green", "") . "	Remove module\n\n");
 
+# Show | List Command
+echo clean_color($colors->getColoredString("[ List Command ] show | list | ls", "brown", "") . "\n");
+
+echo clean_color($colors->getColoredString("ls controller:controllername	", "green", "") . "	List controller\n");
+echo clean_color($colors->getColoredString("ls model:modelname 		", "green", "") . "	List model\n");
+echo clean_color($colors->getColoredString("ls library:libraryname 		", "green", "") . "	List library\n");
+echo clean_color($colors->getColoredString("ls package:packagename 		", "green", "") . "	List package\n");
+echo clean_color($colors->getColoredString("ls extender:extendername 	", "green", "") . "	List extender\n");
+echo clean_color($colors->getColoredString("ls module:modulename 		", "green", "") . "	List module\n\n");
+
+# Explain Command
+echo clean_color($colors->getColoredString("[ Explain Command ] explain | exp", "brown", "") . "\n");
+
+echo clean_color($colors->getColoredString("explain controller:controllername", "green", "") . "	Explain controller\n");
+echo clean_color($colors->getColoredString("explain model:modelname		", "green", "") . "	Explain model\n");
+echo clean_color($colors->getColoredString("explain library:libraryname	", "green", "") . "	Explain library\n");
+echo clean_color($colors->getColoredString("explain package:packagename	", "green", "") . "	Explain package\n");
+echo clean_color($colors->getColoredString("explain extender:extendername	", "green", "") . "	Explain extender\n");
+echo clean_color($colors->getColoredString("explain module:modulename	", "green", "") . "	Explain module\n\n");
+
 # Import Command
-echo clean_color($colors->getColoredString("[ Import Command ]", "brown", "") . "\n");
+echo clean_color($colors->getColoredString("[ Import Command ] import | imp", "brown", "") . "\n");
 
 echo clean_color($colors->getColoredString("import package:packagename	", "green", "") . "	Import package\n");
-echo clean_color($colors->getColoredString("import module:modulename	", "green", "") . "	Import module\n");
+echo clean_color($colors->getColoredString("import module:modulename	", "green", "") . "	Import module\n\n");
+
+# Curl Command
+echo clean_color($colors->getColoredString("[ Curl Command ] curl", "brown", "") . "\n");
+
+echo clean_color($colors->getColoredString("curl get:fullurl		", "green", "") . "	Get url results\n");
+
 echo "\n";

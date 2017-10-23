70d7753e5e6c686df4db1a22ad17cfb3b6998886
phpbala <32deva@gmail.com>
2017-10-23 12:02:24 +0530

    Create DB query builder for error catch DB_STATUS
    Create DB query builder for error catch DB_STATUS

---
+++ database/DB_query_builder_sh.php
@@ -0,0 +1,8 @@
+<?php
+# Fack class by phpbala to find out DB connection
+# Check the DB.php 125 line
+class CI_DB_query_builder extends CI_DB_driver {
+   /*public function __call($name,$method){
+   		die('<p align="center">Database Error : '.$name.'() method is invalid');
+   }*/
+}

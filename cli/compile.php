314e05e58ad53c5d9c8823661b11dc8c7e6a65a2
phpbala <32deva@gmail.com>
2017-10-25 16:01:21 +0530

    Single compile changes
    Single compile changes

--- cli/compile.php
+++ cli/compile.php
@@ -1,7 +1,7 @@
 <?php
 # CLI command import 
 class compile{
-	var $server_uri;
+	
 	public function extender(){ $msg=BAD_FORMAT(); $c_dir = 'extender'; $init_dir = $c_dir.'/init';
 		  
 		  if(!is_dir($init_dir)){ $uold = umask(0); mkdir($init_dir,0777,true); umask($uold); }
@@ -41,6 +41,33 @@ class compile{
 				foreach ($delete as $file) {
 				  unlink($file);
 				}
+				$msg = "\033[0;32m".ucfirst($c_dir)." has been compiled successfully.\nNow you can access the extender routes without call run('extender'). \033[0m \n";   
+
+		  }else{
+		  	$msg = "\033[0;31mPermission denied. coult't import ".$c_dir."  \033[0m \n";
+		  }
+		
+		return $msg;
+	}
+	public function one($extender){ $msg=BAD_FORMAT(); $c_dir = 'extender'; $init_dir = $c_dir.'/init';
+		  
+		  if(!is_dir($init_dir)){ $uold = umask(0); mkdir($init_dir,0777,true); umask($uold); }
+
+		  if(is_dir($init_dir) && is_writable($init_dir)){
+			  	# Move extender files to init folder routes
+		  		$files = glob($c_dir.'/.php');
+				// Identify directories
+				$file = $c_dir."/".strtolower($extender).".php";
+				$destination = $init_dir."/";
+				// Cycle through all source files
+				
+				echo $file."\n";
+				echo "Moving to ".$init_dir." directory...\n";
+				echo $destination.basename($file)."\n";
+				copy($file, $destination.basename($file));
+
+				// Delete all successfully-copied files
+				unlink($file);
 				$msg = "\033[0;32m".ucfirst($c_dir)." has been compiled successfully.\nNow you can access the extender routes without call run('extender'). \033[0m \n";   
 
 		  }else{

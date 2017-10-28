9cf8df4d9466c336f456bff2a5870c743543ad79
phpbala <32deva@gmail.com>
2017-10-28 12:28:26 +0530

    Remote changes
    Remote changes

--- service.php
+++ service.php
@@ -1,9 +1,12 @@
 <?php
 if(isset($_GET['cmd']) && $_GET['cmd']!=''){
-	 $cmd = $_GET['cmd'];
+	 $cmd = strtolower($_GET['cmd']);
 	 $explode = explode("/",$cmd);
-	 if(count($explode) > 0){
-		if($explode[0] == 'rm' || $explode[0] == 'remove'){
+	 if(count($explode) > 0){ echo $base_cmd = $explode[0];
+		if($base_cmd == 'curl'){
+			die("Sorry, Remote curl not allowed.");
+		}
+		if($base_cmd == 'rm' || $base_cmd == 'remove'){
 			$cmd = $cmd." y";
 		}
 	}

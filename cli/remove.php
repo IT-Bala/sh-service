efadf0a3bde5f671cecf660358ff6e13d0fdc917
phpbala <32deva@gmail.com>
2017-10-28 15:00:15 +0530

    remove.php api changes
    remove.php api changes

--- cli/remove.php
+++ cli/remove.php
@@ -1,5 +1,6 @@
 <?php
 # CLI command remove
+require_once 'db.php';
 class remove{
 	public function confirm(){ ob_start();
 		$message   =  "Are you sure want to remove permanently [y/N] :";
@@ -90,5 +91,29 @@ class remove{
 		}
 		return $msg;
 	}
+	public function api($fileName,$dropTable=NULL,$prompt=NULL){ $msg=BAD_FORMAT(); $c_dir = 'extender/init'; $file = $c_dir.'/'.ucfirst($fileName.'.php');
+		$dTc = strtolower($dropTable);
+		if (file_exists($file)){
+			if(is_dir($c_dir) && is_writable($c_dir) && is_readable($file)){ if($prompt!='y') remove::confirm();
+		  		unlink($file);		  		
+		  		if($dTc == '-t' || $dTc == '-table'){
+		  			db()->query("DROP TABLE IF EXISTS `".strtolower($fileName)."`");
+		  		}
+		  		$msg = "\033[0;32m".'API '.ucfirst($fileName).' has been removed permanently'."\033[0m \n";
+		    }else{
+		  		$msg = "\033[0;31mPermission denied. coult't remove ".$c_dir."  \033[0m \n";
+		    }
+		} else {
+  		  $msg = "\033[0;31m".ucfirst($fileName)." ".$c_dir." does not exist.\033[0m \n";
+		  if($dTc == '-t' || $dTc == '-table'){
+		  	if(db()->table_exists(strtolower($fileName))){
+	  			db()->query("DROP TABLE IF EXISTS `".strtolower($fileName)."`");
+	  			$msg = "\033[0;32m".'Table '.strtolower($fileName).' has been removed permanently'."\033[0m \n";
+	  			$msg .= "\033[0;31m".ucfirst($fileName)." ".$c_dir." does not exist.\033[0m \n";
+  			}
+  		  }
+  		}
+  		return $msg;
+  	}
    
 }

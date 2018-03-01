23d07e8939de85dd4fa73732e82dfad733451db4
phpbala <32deva@gmail.com>
2018-03-01 15:22:35 +0530

    Session remove and put changes
    Session remove and put changes

--- Session.php
+++ Session.php
@@ -1,27 +1,38 @@
 <?php
 if(!defined('SHA')) die("Access Denied");
+#session_destroy();
 class Session{
 	# Sessions
 	public function __construct(){
 		if(!isset($_SESSION)){session_start();}
 	}
 	public static function get($variable=NULL){
-		$_= $_SESSION;
+		$_= (isset($_SESSION))?$_SESSION:'';
 		if(trim($variable)!=""){
 			$_ = (isset($_SESSION[$variable]))?$_SESSION[$variable]:'';
 		} return $_;
 	}
 	public static function set($variable=NULL,$value=NULL){
 		if(trim($variable)!=""){
-			echo $_SESSION[$variable] = $value;
+			$_SESSION[$variable] = $value;
 		}
 	}
-	public static function destroy($variable=NULL){
-		session_destroy();
+	public static function put($variable=NULL,$value=NULL){
+		if(trim($variable)!=""){
+			$_SESSION[$variable] = $value;
+		}
 	}
 	public static function delete($variable=NULL){
 		if(trim($variable)!=""){
 			session_unset($variable);
 		}
+	}
+	public static function remove($variable=NULL){
+		if(trim($variable)!=""){
+			session_unset($variable);
+		}
+	}
+	public static function destroy($variable=NULL){
+		session_destroy();
 	}
 }
